b9185d8d3d208f759d8c951725f9b5854a6cb27d
Vladyslav_Dubinin <dubininvladyslav@gmail.com>
2021-08-11 13:24:15 +0300

    add new project

---
+++ app/src/androidTest/java/espressosamples/tutorialspoint/helloworld/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package espressosamples.tutorialspoint.helloworld
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.filters.LargeTest
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class HelloWorldEspressoTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test fun listGoesOverTheFold() {
+        onView(withText("Hello world!")).check(matches(isDisplayed()))
+    }
+}
